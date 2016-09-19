b861723ae46c729b96f6a47eb2215ca4d1b5b3fc
guozhaokui <guozk@layabox.com>
2016-09-19 21:36:06 +0800

    能显示刚体动画

--- laya3d/testAnim.js
+++ laya3d/testAnim.js
@@ -5,9 +5,9 @@ class SkinAnimation_MultiSubMeshSample {
         Laya.stage.screenMode = Laya.Stage.SCREEN_NONE;
         Laya.Stat.show();
         var scene = Laya.stage.addChild(new Laya.Scene());
-        scene.currentCamera = (scene.addChild(new Laya.Camera(0, 0.1, 100)));
-        scene.currentCamera.transform.translate(new Laya.Vector3(0, 0.8, 120.0));
-        scene.currentCamera.transform.rotate(new Laya.Vector3(-30, 0, 0), true, false);
+        scene.currentCamera = (scene.addChild(new Laya.Camera(0, 0.1, 1000)));
+        scene.currentCamera.transform.translate(new Laya.Vector3(0, 0.0, 320.0));
+        scene.currentCamera.transform.rotate(new Laya.Vector3(0, 0, 0), true, false);
         scene.currentCamera.clearColor = null;
         var directionLight = scene.addChild(new Laya.DirectionLight());
         directionLight.direction = new Laya.Vector3(0, -0.8, -1);
@@ -15,8 +15,16 @@ class SkinAnimation_MultiSubMeshSample {
         directionLight.specularColor = new Laya.Vector3(2.0, 2.0, 1.6);
         directionLight.diffuseColor = new Laya.Vector3(1, 1, 1);
         scene.shadingMode = Laya.BaseScene.PIXEL_SHADING;
-        this.skinMesh = scene.addChild(new Laya.MeshSprite3D(Laya.Mesh.load("./laya3d/res/cube/cube-Box001.lm")));
-        this.skinMesh.transform.localRotationEuler = new Laya.Vector3(0, 0, 0);
+        var staticMesh = scene.addChild(new Laya.Sprite3D());
+        staticMesh.once(Laya.Event.HIERARCHY_LOADED, this, function (sender, sprite3D) {
+            //setMeshParams(effectSprite, Laya.Material.RENDERMODE_ADDTIVEDOUBLEFACE);
+            var rootAnimations = sprite3D.addComponent(Laya.RigidAnimations);
+            rootAnimations.url = "./laya3d/res/cube/cube.lani";
+            rootAnimations.player.play(0);
+        });
+        staticMesh.loadHierarchy("./laya3d/res/cube/cube.lh");
+        //this.skinMesh = scene.addChild(new Laya.MeshSprite3D(Laya.Mesh.load("./laya3d/res/cube/cube-Box001.lm")))as Laya.MeshSprite3D;
+        //this.skinMesh.transform.localRotationEuler = new Laya.Vector3(0, 0, 0);
         // this.skinAni = this.skinMesh.addComponent(Laya.SkinAnimations) as Laya.SkinAnimations;
         // this.skinAni.url = "../../res/threeDimen/skinModel/dude/dude.ani";
         // this.skinAni.play();
--- laya3d/testAnim.ts
+++ laya3d/testAnim.ts
@@ -10,9 +10,9 @@ class SkinAnimation_MultiSubMeshSample {
 
         var scene = Laya.stage.addChild(new Laya.Scene()) as Laya.Scene;
 
-        scene.currentCamera = (scene.addChild(new Laya.Camera(0, 0.1, 100))) as Laya.Camera;
-        scene.currentCamera.transform.translate(new Laya.Vector3(0, 0.8, 120.0));
-        scene.currentCamera.transform.rotate(new Laya.Vector3(-30, 0, 0), true, false);
+        scene.currentCamera = (scene.addChild(new Laya.Camera(0, 0.1, 1000))) as Laya.Camera;
+        scene.currentCamera.transform.translate(new Laya.Vector3(0, 0.0, 320.0));
+        scene.currentCamera.transform.rotate(new Laya.Vector3(0, 0, 0), true, false);
         scene.currentCamera.clearColor = null;
 
         var directionLight = scene.addChild(new Laya.DirectionLight()) as Laya.DirectionLight;
@@ -22,9 +22,18 @@ class SkinAnimation_MultiSubMeshSample {
         directionLight.diffuseColor = new Laya.Vector3(1, 1, 1);
         scene.shadingMode = Laya.BaseScene.PIXEL_SHADING;
 
+        var staticMesh = scene.addChild(new Laya.Sprite3D()) as Laya.Sprite3D;
+        staticMesh.once(Laya.Event.HIERARCHY_LOADED, this, function(sender, sprite3D) {
+            //setMeshParams(effectSprite, Laya.Material.RENDERMODE_ADDTIVEDOUBLEFACE);
+            var rootAnimations = sprite3D.addComponent(Laya.RigidAnimations);
+            rootAnimations.url = "./laya3d/res/cube/cube.lani";
+            rootAnimations.player.play(0);
+        });
 
-        this.skinMesh = scene.addChild(new Laya.MeshSprite3D(Laya.Mesh.load("./laya3d/res/cube/cube-Box001.lm")))as Laya.MeshSprite3D;
-        this.skinMesh.transform.localRotationEuler = new Laya.Vector3(0, 0, 0);
+        staticMesh.loadHierarchy("./laya3d/res/cube/cube.lh");        
+
+        //this.skinMesh = scene.addChild(new Laya.MeshSprite3D(Laya.Mesh.load("./laya3d/res/cube/cube-Box001.lm")))as Laya.MeshSprite3D;
+        //this.skinMesh.transform.localRotationEuler = new Laya.Vector3(0, 0, 0);
         // this.skinAni = this.skinMesh.addComponent(Laya.SkinAnimations) as Laya.SkinAnimations;
         // this.skinAni.url = "../../res/threeDimen/skinModel/dude/dude.ani";
         // this.skinAni.play();
