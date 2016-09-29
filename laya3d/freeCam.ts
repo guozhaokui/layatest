1c5f7058137ae78e54e479e4575c4314253987d0
guozhaokui <guozk@layabox.com>
2016-09-29 22:20:11 +0800

    测试单位：创建一个1的球，可以看到导出的size=1的盒子是对的。为了一致，max要设置system unit为1=1m 如果为inch则1就是0.025m

--- laya3d/freeCam.ts
+++ laya3d/freeCam.ts
@@ -1,13 +1,22 @@
 
 class FreeCamera extends Laya.Camera{
-    v3Trans = new Laya.Vector3();
+    vTrans = new Laya.Vector3();
+    vRot = new Laya.Vector3(0,0.1,0);
     constructor(aspR:number, n:number,f:number){
         super(aspR,n,f);
     }
 
     _update(rs:Laya.RenderState){
         super._update(rs);
-        this.v3Trans.z=0.1;
-        this.transform.translate(this.v3Trans);
+        //this.vTrans.z=0.1;
+        this.transform.translate(this.vTrans);
+    }
+    rotl(){
+        this.vRot.y=0.1;
+        this.transform.rotate(this.vRot);
+    }
+    rotr(){
+        this.vRot.y=-0.1;
+        this.transform.rotate(this.vRot);
     }
 }
--- laya3d/game.ts
+++ laya3d/game.ts
@@ -1,11 +1,15 @@
 ///<reference path="gamesce.ts" />
 /// <reference path="shape/cylinder.ts" />
+/// <reference path="character.ts" />
 
 
+var Vec3 = Laya.Vector3;
 class GameTest{
     sceRoot:Laya.Scene=null;
     gameScene:GameScene=null;
-    currCam:Laya.Camera=null;
+    currCam:FreeCamera=null;
+    player1:Laya.Sprite3D=null;
+    npcs:Laya.Sprite3D[]=[];
     constructor(){
         this.initEngine();
         this.sceRoot = Laya.stage.addChild(new Laya.Scene()) as Laya.Scene;
@@ -16,24 +20,62 @@ class GameTest{
         Laya.stage.scaleMode = Laya.Stage.SCALE_FULL;
         Laya.stage.screenMode = Laya.Stage.SCREEN_NONE;
         Laya.Stat.show();
+        Laya.stage.on(Laya.Event.KEY_DOWN,this,this.handlKeyEvent);
     }
 
     loadScene(){
-        var defCam =this.currCam=  this.sceRoot.addChild(new FreeCamera(1,0.1,1000)) as Laya.Camera;
+        var defCam =this.currCam=  this.sceRoot.addChild(new FreeCamera(1,0.1,1000)) as FreeCamera;
         this.sceRoot.currentCamera = defCam;
-        defCam.transform.translate(new Laya.Vector3(0,0,0));
+        defCam.transform.translate(new Laya.Vector3(0,0.0,1.0));
+        //defCam.transform.rotate(new Laya.Vector3(-30,0,0),true,false);
         this.gameScene=new GameScene(this.sceRoot);
         this.sceRoot.addChild(this.gameScene);
         this.gameScene.load('laya3d/res/scene/scene.lh');
+        //this.gameScene.transform.localScale= new Vec3(1.0/2.54,1.0/2.54,1.0/2.54);
 
-        var c1 = new _Cylinder(1,1,10,10);
-        this.sceRoot.addChild( new Laya.MeshSprite3D(c1));
+        //var c1 = new _Cylinder(0.09,0.18,10,10);
+        //this.sceRoot.addChild( new Laya.MeshSprite3D(c1));
+        var s1 = new Laya.Sphere(1,10,10);
+        var s1sp = this.sceRoot.addChild(new Laya.MeshSprite3D(s1)) as Laya.Sprite3D;
+        s1sp.transform.translate(new Vec3(-2,-0.0,-16.9));
+    }
+
+    addNPC(x:number,y:number,z:number, dir:number){
+
+    }
+
+    addPlayer(x:number,y:number,z:number, dir:number):GameTest{
+        var npc = new game_character(this.sceRoot);
+        this.sceRoot.addChild(npc);
+        this.npcs.push(npc);
+        npc.transform.position=new Vec3(x,y,z);
+        return this;
     }
 
     start(){
         this.loadScene();
+        this.addPlayer(0,0,0,0)
+            .addPlayer(1,0,0,0)
+            .addPlayer(2,0,0,0)
+            .addPlayer(-1,0,0,0);
+        for(var i=0; i<20; i++){
+            this.addPlayer(Math.random()*20-10,0,Math.random()*10-5,0);            
+        }
     }
     
+    handlKeyEvent(e:KeyboardEvent){
+        console.log(e.keyCode);
+        switch(e.keyCode){
+            case 37://left
+                this.currCam.rotl();
+            break;
+            case 39://right
+                this.currCam.rotr();
+            break;
+        }
+        //if(e.keyCode)
+        //this.currCam.rotl();
+    }
 }
 
 var game = new GameTest();
